15447b9a8f85512301375a02d0fef559d3ff5b9c
Md Irshad <m3irshad3@gmail.com>
2023-08-15 16:32:36 +0530

    EDA and model training done in notebook

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     """Whenever the error raise in my code this function will be called. This function gives on which file and which line no error occurs
@@ -17,5 +17,9 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_msg
+    
+
+    
+    
 
     
