c787b9e94b04087115c014eefc88ac69136bae4a
Md Irshad <m3irshad3@gmail.com>
2023-08-21 05:57:08 +0530

    Added Hyperperamater tunnning for all model

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -25,12 +26,19 @@ def get_metrics(actual_value,preidct_value):
     return (mse,rmse,r2_score_value)
 
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         model_report = {}
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para  = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            #Give the best parmter value to the model
+            model.set_params(**gs.best_params_)
             
             #Fit the model to train data
             model.fit(X_train,y_train)
